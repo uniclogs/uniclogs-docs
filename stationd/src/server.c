59291e015ed5d85e3f112b3aa4558ee660a7a8df
Miles Simpson <miles.a.simpson@gmail.com>
2018-11-01 22:22:03 -0700

    Initial commit of stationd server thread files

---
+++ stationd/src/server.c
@@ -0,0 +1,76 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server.c
+ *
+ *    Description:  stationd UDP server implementation
+ *
+ *        Version:  0.1
+ *        Created:  10/25/2018 03:15:54 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Miles Simpson (heliochronix), miles.a.simpson@gmail.com
+ *   Organization:  PSAS
+ *
+ * =====================================================================================
+ */
+
+#include "server.h"
+
+#define MAXBUF 2048
+#define PORT "8080"
+
+void *udp_serv(void *argp)
+{
+    int sd = -1, status, recvlen;
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage remaddr;
+    socklen_t addrlen;
+    unsigned char buf[MAXBUF];
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo))) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(EXIT_FAILURE);
+    }
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) {
+            perror("Error: Cannot create socket\n");
+            continue;
+        }
+        if (bind(sd, p->ai_addr, p->ai_addrlen) < 0) {
+            close(sd);
+            perror("Error: Bind failed\n");
+            continue;
+        }
+        break;
+    }
+    if (p == NULL) {
+        fprintf(stderr, "Error: Failed to bind socket\n");
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(servinfo);
+
+    addrlen = sizeof(remaddr);
+    while (1) {
+        printf("Waiting on port %s...\n", PORT);
+        if ((recvlen = recvfrom(sd, buf, MAXBUF - 1, 0, (struct sockaddr *)&remaddr, &addrlen)) < 0)
+        {
+            perror("Error: Receive failure\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("Received %d bytes\n", recvlen);
+        if (recvlen > 0) {
+            buf[recvlen] = '\0';
+            printf("Received message: \"%s\"\n", buf);
+        }
+    }
+
+    close(sd);
+    pthread_exit(NULL);
+}
---
+++ stationd/src/server.h
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server.h
+ *
+ *    Description:  stationd UDP server header
+ *
+ *        Version:  0.1
+ *        Created:  10/25/2018 03:15:54 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Miles Simpson (heliochronix), miles.a.simpson@gmail.com
+ *   Organization:  PSAS
+ *
+ * =====================================================================================
+ */
+
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+void *udp_serv(void *argp);
+
+#endif
