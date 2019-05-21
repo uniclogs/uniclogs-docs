10f722f6ee3b1470d7f3011326ed9a9154e137ce
Miles Simpson <miles.a.simpson@gmail.com>
2019-05-21 13:34:06 -0700

    First attempt at ADS1115 value reading

--- stationd/src/ads1115.c
+++ stationd/src/ads1115.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/ioctl.h>
@@ -7,6 +8,8 @@
 #include "common.h"
 #include "ads1115.h"
 
+static int retries;
+
 void ADS1115SetSlave(int i2c_fd)
 {
 	/* Set ADS1115 as slave device */
@@ -14,4 +17,42 @@ void ADS1115SetSlave(int i2c_fd)
 		logmsg(LOG_ERR, "Error: Failed setting ADS1115 as slave: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
+}
+
+int16_t ADS1115ReadVal(int i2c_fd, uint8_t sensor)
+{
+	int16_t regval = 0;
+	logmsg(LOG_DEBUG, "Reading value of sensor number: %u\n", sensor);
+	if (sensor > 3) {
+		logmsg(LOG_WARNING, "Invalid sensor number!\n");
+		return 0;
+	}
+	ADS1115SetSlave(i2c_fd);
+
+	logmsg(LOG_DEBUG, "Reading current config register state...\n");
+	if ((regval = i2c_smbus_read_word_data(i2c_fd, ADS1115_CONFIG_REG)) < 0) {
+		logmsg(LOG_ERR, "Error: Failed reading current config register state: %s\n", strerror(errno));
+		return 0;
+	}
+	logmsg(LOG_DEBUG, "Read 0x%04X...\n", regval);
+
+	regval &= ~(0xF << 12);
+	regval |= ADS1115_START_CONV(sensor);
+
+	logmsg(LOG_DEBUG, "Writing 0x%04X...\n", regval);
+	for (retries = 2; retries > 0 && i2c_smbus_write_word_data(i2c_fd, ADS1115_CONFIG_REG, regval) < 0; retries--)
+		usleep(100);
+	if (retries == 0) {
+		logmsg(LOG_ERR, "Error: Failed starting conversion of sensor %u: %s\n", sensor, strerror(errno));
+		return 0;
+	}
+	usleep(100);
+	logmsg(LOG_DEBUG, "Reading conversion value...\n");
+	if ((regval = i2c_smbus_read_word_data(i2c_fd, ADS1115_CONV_REG)) < 0) {
+		logmsg(LOG_ERR, "Error: Failed reading conversion value: %s\n", strerror(errno));
+		return 0;
+	}
+	logmsg(LOG_DEBUG, "Read 0x%04X...\n", regval);
+
+	return regval;
 }
--- stationd/src/ads1115.h
+++ stationd/src/ads1115.h
@@ -4,8 +4,15 @@
 #include <stdint.h>
 #include "common.h"
 
-/* Register values for ADS1115 (IOCON.BANK = 0) */
+/* Register values for ADS1115 */
+#define ADS1115_CONV_REG	0x00
+#define ADS1115_CONFIG_REG	0x01
+#define ADS1115_LO_REG		0x02
+#define ADS1115_HIGH_REG	0x03
+
+#define ADS1115_START_CONV(n)	((0xC + n) << 12)
 
 void ADS1115SetSlave(int i2c_fd);
+int16_t ADS1115ReadVal(int i2c_fd, uint8_t sensor);
 
 #endif
