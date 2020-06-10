6565b775ed81d8a8d0cf18e3efb2db33151363da
pentuple <djernaes@pdx.edu>
2020-06-10 13:37:18 -0700

    updated format and small fixes

--- cosmos/spacetrack/spaceTrack.py
+++ cosmos/spacetrack/spaceTrack.py
@@ -1,62 +1,54 @@
 ## connect to the Space-Track.org API
 ## spaceTrack.py
-
-## Code adapted from 
-## (c) 2019 Andrew Stokes  All Rights Reserved
-# See https://www.space-track.org/documentation
+## need space-track.org un/pw credentials and norad_id for satellite
 
 import requests
 import json
-import time
-from datetime import datetime
-import os, argparse
+import os, sys
+
+
+def credentials(usr, pwd):
+    siteCred = {'identity': usr, 'password': pwd}
+    return siteCred
+
+def get_Norad_id(norad_id):
+    return norad_id
 
 class MyError(Exception):
     def __init___(self,args):
-        Exception.__init__(self,"my exception was raised with arguments {0}".format(args))
+        Exception.__init__(self,"Exception raised with arguments {0}".format(args))
         self.args = args
 
+# norad_id = 25544 for ISS (ZARYA)
+def getTLE(siteCred, norad_id):
 
-uriBase                = "https://www.space-track.org"
-requestLogin           = "/ajaxauth/login"
-requestCmdAction       = "/basicspacedata/query"
-requestFindStarlinks   = "/class/tle/NORAD_CAT_ID/25544/orderby/EPOCH desc/limit/5/format/json"
+    uriBase                = "https://www.space-track.org"
+    requestLogin           = "/ajaxauth/login"
+    requestTLE             = "/basicspacedata/query/class/tle/NORAD_CAT_ID/"
+    sat_id                 = norad_id
+    requestDetails         ="/predicates/TLE_LINE1,TLE_LINE2/limit/1/format/json"
 
+    with requests.Session() as session:
+        resp = session.post(uriBase + requestLogin, data = siteCred)
+        if resp.status_code != 200:
+            raise MyError(resp, "Credentials fail on login")
 
-""" for testing purposes here are space-track credentials"""
-#configUsr = 'djernaes@pdx.edu'
-#configPwd = 'NrXwf-mYc6Hx2cZ'
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("username", help="arg1 = username")
-parser.add_argument("password", help="arg2 = password")
-args = parser.parse_args()
-configUsr = args.username
-configPwd = args.password
-
-siteCred = {'identity': configUsr, 'password': configPwd}
+        resp = session.get(uriBase + requestTLE + str(norad_id) + requestDetails)
+        if resp.status_code != 200:
+            print(resp)
+            raise MyError(resp, "TLE request fails, check credentials")
 
 
-# use requests package to drive the RESTful session with space-track.org
-with requests.Session() as session:
-    # run the session in a with block to force session to close if we exit
-
-    # need to log in first. note that we get a 200 to say the web site got the data, not that we are logged in
-    resp = session.post(uriBase + requestLogin, data = siteCred)
-    if resp.status_code != 200:
-        raise MyError(resp, "POST fail on login")
-
-    # this query picks up all Starlink satellites from the catalog. Note - a 401 failure shows you have bad credentials 
-    resp = session.get(uriBase + requestCmdAction + requestFindStarlinks)
-    if resp.status_code != 200:
-        print(resp)
-        raise MyError(resp, "GET fail on request for Starlink satellites")
-
-    # output to file in json format
-    retData = json.loads(resp.text)
-    with open('jsonOutput.txt', 'w') as outFile:
-        json.dump(retData, outFile)
+        # output to file in json format
+        retData = json.loads(resp.text)
+        with open('jsonOutput.json', 'w') as outFile:
+            json.dump(retData, outFile)
+        # or just return data from function    
+        print("process complete")
+    return retData
 
 
-print("Completed session") 
+if __name__ == "__main__":
+    siteCred = credentials(usr, pwd)
+    norad_id = get_Norad_id(25544)
+    tleOutput = getTLE(siteCred, norad_id)
