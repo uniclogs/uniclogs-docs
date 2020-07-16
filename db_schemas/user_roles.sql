fbb594634318c08d695c62304b7da01684ec00d8
Ismail Kably <sharp-man@live.fr>
2020-07-16 05:26:47 -0700

    Remove role script

--- db_schemas/user_roles.sql
+++
@@ -1,7 +0,0 @@
-/*Please note that these password have to be replaced during deployement.
-  The following passwords are merely provided to ease developement and test
-*/
-
-CREATE ROLE cosi WITH LOGIN ENCRYPTED PASSWORD 'password1';
-CREATE ROLE rads WITH LOGIN ENCRYPTED PASSWORD 'password2';
-CREATE ROLE ultra WITH LOGIN ENCRYPTED PASSWORD 'password3';
