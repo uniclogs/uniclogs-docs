4af9a3045e022e510a30144312a7dfa5bece8845
yang4 <yang4@pdx.edu>
2020-07-18 18:48:38 -0700

    Working on adding pads to progam and displaying SQL alchemy statements

--- rads/director.py
+++ rads/director.py
@@ -16,7 +16,8 @@ from sqlalchemy import Column, \
 from sqlalchemy.ext.declarative import declarative_base
 from loguru import logger
 import log_interface
-from .commonrads import PSQL_USERNAME, \
+                    
+from commonrads import PSQL_USERNAME, \
                     PSQL_PASSWORD, \
                     EnvironmentVariableNotDefined
   
@@ -60,6 +61,86 @@ def print_menu(stdscreen,menu, current_row_index):
 
     stdscreen.refresh()
 
+def print_pad(panel, stdscreen, menu, current_row_index):
+    """Prints main menu and updates the display to current row/option selected.
+
+    Parameters
+    ----------
+    stdscreen : window object
+        A windows object initialized by curses.initscr() from the curses library.
+    menu: list
+        A list that contains all text for the main menu options.
+    current_row_index: int
+        The current index row number of the current menu option selected.
+    Returns
+    -------
+    None
+    """
+
+    stdscreen.clear()
+    #gets the max height and width
+    h, w = stdscreen.getmaxyx()
+
+    #enumerate loops over menu and creates a counter to know what part of the menu is selected
+    for index, row in enumerate(menu):
+        #X is equal to center of screen with text alignment of the x axis
+        x = w//2 - len(row)//2
+        #y is equal to the center of the screen of the axis
+        y = h//2 - len(menu)//2 + index
+        #highlights the selected text
+        if index == current_row_index:
+            panel.attron(curses.color_pair(1))
+            panel.addstr(y, x, row)
+            panel.attroff(curses.color_pair(1))
+        #adds menu text not selected
+        else:
+            panel.addstr(y, x, row)
+
+#    panel.refresh(0,0, 5,5, 20,75)
+
+#function to print schedule of upcoming requests
+def schedule(stdscreen):
+    """Prints main menu and updates the display to current row/option selected.
+
+    Parameters
+    ----------
+    Returns
+    -------
+    None
+    """
+    schedule_index = 0
+    stdscreen.clear()
+    list1 = ['op1','op2','op3','op4','op5','op6','op7','op8','op9','op10']
+    vheight, vwidth = stdscreen.getmaxyx()
+    panel = curses.newpad(vheight -1, vwidth-2)
+ #   print_pad(panel, stdscreen, list1, schedule_index)
+    print_menu(stdscreen, list1, schedule_index)
+    while True:
+        #interprets arrow key strokes
+        key = stdscreen.getch()
+        stdscreen.clear()
+
+        #menu navigation
+        #lower bound case
+        if key == curses.KEY_UP and schedule_index > 0:
+            schedule_index-=1
+        #upper bound case
+        elif key == curses.KEY_DOWN and schedule_index < len(list1)-1:
+            schedule_index += 1
+        elif key == curses.KEY_F1:
+            stdscreen.clear()
+            stdscreen.addstr(0, 0, "You have pressed the F1 key!")
+            stdscreen.refresh()
+            time.sleep(0.75)
+            break
+        #all possible values that enter key might be depending on keyboard
+        elif key == curses.KEY_ENTER or key in [10,13]:
+            stdscreen.clear()
+            stdscreen.addstr(0, 0, "You Selected {}".format(list1[schedule_index]))
+            stdscreen.refresh()
+            stdscreen.getch()
+        print_menu(stdscreen, list1, schedule_index)
+        stdscreen.refresh() 
 def main():
 
     log_interface.init('rads')
@@ -94,15 +175,22 @@ def main():
         #upper bound case
         elif key == curses.KEY_DOWN and current_row_index < len(menu)-1:
             current_row_index += 1
-        elif key == curse
+        elif key == curses.KEY_F1:
+            stdscreen.clear()
+            stdscreen.addstr(0, 0, "You have pressed the F1 key!")
+            stdscreen.refresh()
+            stdscreen.getch()
         #all possible values that enter key might be depending on keyboard
         elif key == curses.KEY_ENTER or key in [10,13]:
-            #code for selecting menu from menu list options 0 = Approve/Deny, 1 = Check Schedule = 2,  Archive = 3, Exit = 4 
-            if current_row_index < len(menu)-1:
-                stdscreen.clear()
-                stdscreen.addstr(0, 0, "You Selected {}".format(menu[current_row_index]))
-                stdscreen.refresh()
-                stdscreen.getch()
+            #code for selecting menu from menu list options 0 = Approve/Deny, 1 = Check Schedule, 2 =  Archive, 3 = Exit 
+            #check schedule
+            if current_row_index == 1:
+                schedule(stdscreen)
+           # if current_row_index < len(menu)-1:
+            #    stdscreen.clear()
+             #   stdscreen.addstr(0, 0, "You Selected {}".format(menu[current_row_index]))
+              #  stdscreen.refresh()
+              #  stdscreen.getch()
             #code that terminates the program after selecting exit
             if current_row_index == len(menu)-1:
                 stdscreen.addstr(0, 0, "Program successfully closed!")
