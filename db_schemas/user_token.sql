58a58a39087bd227b3c1afc74876bf6fe843c1c2
MelanchoŁia <z-best-@hotmail.com>
2020-07-22 12:47:19 -0700

    Fix typo in the user_token table

--- db_schemas/user_token.sql
+++ db_schemas/user_token.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_token
 (
     token text NOT NULL,
-    "user-id" character varying(155),
+    user_id character varying(155),
     CONSTRAINT user_token_pkey PRIMARY KEY (token),
     CONSTRAINT user_token_token_fkey FOREIGN KEY (token)
         REFERENCES public.requests (user_token) MATCH SIMPLE
