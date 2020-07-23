3c74e10da575b7cd6537ca7a690e813e0f57dbb3
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-07-23 15:05:54 -0700

    First working version of telemetry injection.

--- cosi/cosid.py
+++ cosi/cosid.py
@@ -20,14 +20,31 @@ def cycle(norad_id):
     print('Telemetry: ' + status)
     if(status == 'OK'):
         decoded_telemetry = satnogs.decode_telemetry_frame(encoded_telemetry)
-        print('Decoded Telemetry: ' + str(decoded_telemetry))
-        b.inject_tlm('ENGR_LINK', 'TLM TEMPS TEMP1 = 300, TEMP2 = 400, TIMESTAMP = 00320203')
+
+        # Get data from DART
+        packet = b.get_tlm_packet('ENGR_LINK', 'MAGNETOMETER')
+        print('Got magentometer packets from DART: {}'.format(packet))
+
+        # Send data to dart
+        magentometer_telemetry = {
+            'INVALID_COUNT': decoded_telemetry.mag_invalid_count,
+            'SENSOR_USED': decoded_telemetry.mag_sensor_used,
+            'VECTOR_BODY1': decoded_telemetry.mag_vector_body1,
+            'VECTOR_BODY2': decoded_telemetry.mag_vector_body2,
+            'VECTOR_BODY3': decoded_telemetry.mag_vector_body3,
+            'VECTOR_VALID': decoded_telemetry.mag_vector_valid
+        }
+        try:
+            b.inject_tlm('ENGR_LINK', 'MAGNETOMETER', item_hash=magentometer_telemetry)
+            print('Succesfully injected telemetry frame into DART: {}'.format(magentometer_telemetry))
+        except Exception:
+            print('Failed to inject telemetry frame into DART: {}'.format(magentometer_telemetry))
 
 
 def main(args):
     if(len(args) == 0):
-        print('Expected Norad ID!')
-        sys.exit(-1)
+        args = [43793]
+        print('No Norad ID provided, defaulting to using CSIM!')
     for id in args:
         try:
             id = int(id)
