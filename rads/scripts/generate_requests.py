108b88d2173458abc9bfa31b3105092da04cda48
ryanpdx <rmedick@pdx.edu>
2020-07-30 12:47:25 -0700

    more randomness for request_generator and faster runtime

--- rads/scripts/generate_requests.py
+++ rads/scripts/generate_requests.py
@@ -9,6 +9,24 @@ sys.path.insert(0, '../..')
 import pass_calculator.calculator as pc
 
 
+tle = [
+    "1 25544U 98067A   20185.75040611  .00000600  00000-0  18779-4 0  9992",
+    "2 25544  51.6453 266.4797 0002530 107.7809  36.4383 15.49478723234588"
+    ]
+
+# oregon as a box
+MinLongitude = -116.926326
+MaxLatitude = 46.211947
+MaxLongitude = -124.001521
+MinLatitude = 41.990853
+
+
+days_in_past = 2
+start = datetime.now()
+start = start - timedelta(days=days_in_past)
+start = start.replace(tzinfo=timezone.utc)
+
+
 def randomword(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
@@ -17,24 +35,12 @@ def randomword(length):
 def generated_passes(num: int):
     generated_passes = []
 
-    tle = [
-        "1 25544U 98067A   20185.75040611  .00000600  00000-0  18779-4 0  9992",
-        "2 25544  51.6453 266.4797 0002530 107.7809  36.4383 15.49478723234588"
-        ]
-
-    # oregon as a box
-    MinLongitude = -116.926326
-    MaxLatitude = 46.211947
-    MaxLongitude = -124.001521
-    MinLatitude = 41.990853
-
-    now = datetime.now()
-    now = now - timedelta(days=1)
-    now = now.replace(tzinfo=timezone.utc)
-    future = now + timedelta(days=7)
-    future = future.replace(tzinfo=timezone.utc)
-
     for i in range(num):
+        days = random.randrange(days_in_past+7)
+        start_day = start + timedelta(days=days)
+        start_day = start_day.replace(tzinfo=timezone.utc)
+        end_day = start_day + timedelta(days=1)
+        end_day = end_day.replace(tzinfo=timezone.utc)
 
         latitude = round(random.uniform(MinLatitude, MaxLatitude), 6)
         longitude = round(random.uniform(MinLongitude, MaxLongitude), 6)
@@ -45,8 +51,8 @@ def generated_passes(num: int):
                 tle=tle,
                 lat_deg=latitude,
                 long_deg=longitude,
-                start_datetime_utc=now,
-                end_datetime_utc=future,
+                start_datetime_utc=start_day,
+                end_datetime_utc=end_day,
                 elev_m=elevation_m
                 )
 
@@ -94,12 +100,20 @@ def insert_into_db(generated_passes, random_status=False):
             approved_status = None
             sent_status = False
 
+        # make a random created_dt
+        days = random.randrange(14)
+        mins = random.randrange(60)
+        secs = random.randrange(60)
+        created_dt = start - timedelta(days=days, minutes=mins, seconds=secs)
+        created_dt = created_dt.replace(tzinfo=timezone.utc)
+
         new_request = Request(
             user_token=randomword(10),
             is_approved=approved_status,
             is_sent=sent_status,
             pass_uid=pass_uid,
-            observation_type=random.choice(["cfc", "oresat live", "uniclogs"])
+            observation_type=random.choice(["cfc", "oresat live", "uniclogs"]),
+            created_date=created_dt
             )
 
         session.add(new_request)
@@ -150,7 +164,7 @@ if __name__ == '__main__':
             Flags
             -h   : help message
             -r   : randomize is_approved and is_sent statuses
-            -n x : generate x passes
+            -n x : generate x passes where x > 0 and x < 10000
             -c   : clean db
             None : gererate 100 non randomize requests
             """)
@@ -161,7 +175,11 @@ if __name__ == '__main__':
         elif o == '-r':
             random_status = True
         elif o == '-n':
-            num = int(a)
+            v = int(a)
+            if v < 0 or v >= 10000:
+                print("Can't be negative or greater than 10000")
+                exit(0)
+            num = v
         else:
             print("Unkown flag, run with -h for help message")
             exit(1)
