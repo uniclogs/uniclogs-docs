6b33d1b3eebc8be07e1d161f240625c3800dfed9
David Djernaes <djernaes@pdx.edu>
2020-08-04 14:30:10 -0700

    minor fix. will squash this with other commits

--- cosi/schedule_pass.py
+++ cosi/schedule_pass.py
@@ -34,7 +34,6 @@ class Schedule_Pass:
          
             Note: only the pass_uid, latitude, longitude, start_time will be 
             sent to the satellite
-request = ('user_token', 0293,'pass_uid', 93.402, -3.494,'is_approved', True)
         numberOfRequests : number of passes to be scheduled or canceled
         """
         self.passRequestList = request
