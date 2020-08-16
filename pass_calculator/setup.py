dfb7ab4db7a4f46a87dc6413899ae821602711ba
ryanpdx <rmedick@pdx.edu>
2020-08-15 18:39:36 -0700

    fixed pypi version/license

--- pass_calculator/setup.py
+++ pass_calculator/setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as f:
 
 setup(
     name='pass_calculator',
-    version='1.1',
+    version='1.0',
     description='A common pass calculator for ULTRA and RADS',
     long_description='A common pass calculator for ULTRA and RADS',
-    license="GPL v3",
+    license="GPLv3",
     author='psas',
     author_email='oresat@pdx.edu',
     url="https://github.com/oresat/uniclogs-software",
@@ -16,7 +16,7 @@ setup(
     install_requires=['skyfield'],
     classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
+         "OSI Approved :: GNU General Public License v3 (GPLv3)"
          "Operating System :: OS Independent",
      ],
 )
